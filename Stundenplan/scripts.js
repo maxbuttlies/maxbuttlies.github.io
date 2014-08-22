3c5f0d1969d395fc68d999740be35b6ebd023c69
Maximilian Buttlies <hey@buttli.es>
2014-08-22 13:15:19 +0200

    Publish 'Stundenplan' as site.

---
+++ Stundenplan/scripts.js
@@ -0,0 +1,38 @@
+(function() {
+  var app;
+
+  app = angular.module("schedule", []);
+
+  app.controller('ScheduleController', function($scope, $http, $location, $filter) {
+    $scope.today = new Date();
+    $scope.is_lesson = function(time, day) {
+      var datestring, lesson_date, lesson_date_end;
+      if ($scope.is_today(day)) {
+        console.log(typeof cday + '/' + typeof day);
+        datestring = $filter('date')($scope.today, 'MMM dd, yyyy') + ' ' + time;
+        lesson_date = new Date(datestring);
+        lesson_date_end = new Date(lesson_date.getTime() + 45 * 60000);
+        return lesson_date.getTime() < $scope.today.getTime() && lesson_date_end.getTime() > $scope.today.getTime();
+      }
+    };
+    $scope.is_today = function(day) {
+      return $scope.today.getDay() === parseInt(day, 10);
+    };
+    $http.get('api/units.json').success(function(data, status, headers, config) {
+      return $scope.units = data;
+    });
+    return $http.get('api/days.json').success(function(data, status, headers, config) {
+      $scope.days = data.days;
+      $scope.weeks = data.weeks;
+      $scope.times = data.times;
+      $scope.week_number = $filter('date')($scope.today, 'w');
+      $scope.$on('$locationChangeSuccess', function() {
+        return $scope.current_week = $location.search().week;
+      });
+      if ($scope.current_week === '' || $scope.current_week === void 0) {
+        return $scope.current_week = $scope.weeks[$scope.week_number % 2];
+      }
+    });
+  });
+
+}).call(this);
